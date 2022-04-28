f6be70b2b5b2ceeed973dd7561fb34d43c661dd7
tgangemi <97982017+tgangemi@users.noreply.github.com>
2022-04-28 18:08:01 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// (function(){
+//     var words = [
+//         ' websites',
+//         ' stories',
+//         ' books',
+        
+//         ], i = 0;
+//     setInterval(function(){
+//         $('#changingword').fadeOut(function(){
+//             $(this).html(words[i=(i+1)%words.length]).fadeIn();
+//         });
+//     }, 3000);
+      
+// })();
+
